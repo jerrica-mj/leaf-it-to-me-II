acc45a42ec8135726e864619d370b14cd232ebf8
JJ <jerrica.mj@gmail.com>
2021-04-04 00:46:37 -0700

    Add styling to material-ui components.

--- react-front-end/src/components/navbar.js
+++ react-front-end/src/components/navbar.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles"
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const Navbar = () => {
+  const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar>
         <IconButton edge="start"
-          // className={classes.menuButton}
+          className={classes.menuButton}
           color="inherit"
           aria-label="menu"
         >
@@ -15,7 +31,7 @@ const Navbar = () => {
         </IconButton>
         <Typography
           variant="h6"
-          // className={classes.title}
+          className={classes.title}
         >
           News
         </Typography>
