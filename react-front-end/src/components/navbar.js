b71ed27423aac4d3aefbc27ef547b88c6b330bca
JJ <jerrica.mj@gmail.com>
2021-04-04 00:41:30 -0700

    Update navbar to use material-ui.

--- react-front-end/src/components/navbar.js
+++ react-front-end/src/components/navbar.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import MenuIcon from "@material-ui/icons/Menu";
 
-
-export default function Navbar(props) {
+const Navbar = () => {
   return (
-    <>
-      <p>This is the navbar</p>
-    </>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start"
+          // className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          variant="h6"
+          // className={classes.title}
+        >
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
   );
-};
+}
+
+export default Navbar;
